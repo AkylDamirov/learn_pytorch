f5b1640506f6fae96c487d1700ae24ec3a47f75e
Akyl <akyl@gmail.com>
2024-05-19 18:19:55 +0200

    19

--- 04_pytorch_custom_datasets.py
+++ 04_pytorch_custom_datasets.py
@@ -561,7 +561,209 @@ except:
     import torchinfo
 
 from torchinfo import summary
-summary(model_0, input_size=[1, 3, 64, 64]) # do a test pass through of an example input size
+# summary(model_0, input_size=[1, 3, 64, 64]) # do a test pass through of an example input size
+
+
+# 7.5 Create train & test loop functions
+def train_step(model:torch.nn.Module,
+               dataloader: torch.utils.data.DataLoader,
+               loss_fn: torch.nn.Module,
+               optimizer: torch.optim.Optimizer):
+    #put model in train mode
+    model.train()
+
+    #setup train loss and loss accuracy values
+    train_loss, train_acc = 0,0
+
+    #loop through data loader data batches
+    for batch, (X, y) in enumerate(dataloader):
+        #send data to target device
+        X,y = X.to(device), y.to(device)
+
+        #forward pass
+        y_pred = model(X)
+
+        #calculate and accumulate loss
+        loss = loss_fn(y_pred, y)
+        train_loss += loss.item()
+
+        #optimizer zero grad
+        optimizer.zero_grad()
+
+        #loss backward
+        loss.backward()
+
+        #optimizer step
+        optimizer.step()
+
+        #calculate and accumulate accuracy metric across all batches
+        y_pred_class = torch.argmax(torch.softmax(y_pred, dim=1), dim=1)
+        train_acc += (y_pred_class==y).sum().item() / len(y_pred)
+
+    #adjust metrics to get average loss and accuracy per batch
+    train_loss = train_loss / len(dataloader)
+    train_acc = train_acc / len(dataloader)
+    return train_loss, train_acc
+
+
+def test_step(model: torch.nn.Module,
+              dataloader: torch.utils.data.DataLoader,
+              loss_fn: torch.nn.Module):
+    #put model in eval mode
+    model.eval()
+
+    #setup test loss and test accuracy values
+    test_loss, test_acc = 0, 0
+
+    #turn on inference context manager
+    with torch.inference_mode():
+        #loop through Dataloader batches
+        for bacth, (X, y) in enumerate(dataloader):
+            #send data to target device
+            X,y = X.to(device), y.to(device)
+
+            #forward pass
+            test_pred_logits = model(X)
+
+            #calculate and accumulate loss
+            loss = loss_fn(test_pred_logits, y)
+            test_loss += loss.item()
+
+            #calculate and accumulate accuracy
+            test_pred_labels = test_pred_logits.argmax(dim=1)
+            test_acc += ((test_pred_labels == y).sum().item() / len(test_pred_labels))
+
+        #adjust metrics to get average loss and accuracy per batch
+        test_loss =  test_loss / len(dataloader)
+        test_acc = test_acc / len(dataloader)
+        return test_loss, test_acc
+
+# 7.6 Creating a train() function to combine train_step() and test_step()
+from tqdm.auto import tqdm
+
+#take various parameters required for training and test steps
+def train(model: torch.nn.Module,
+          train_dataloader: torch.utils.data.DataLoader,
+          test_dataloader: torch.utils.data.DataLoader,
+          optimizer: torch.optim.Optimizer,
+          loss_fn: torch.nn.Module = nn.CrossEntropyLoss(),
+          epochs: int = 5):
+
+    #create empty results dictionary
+    results = {'train_loss': [],
+               'train_acc': [],
+               'test_loss':[],
+               'test_acc': []}
+
+    #loop through training and testing steps for a number of epochs
+    for epoch in tqdm(range(epochs)):
+        train_loss, train_acc = train_step(model=model,
+                                           dataloader=train_dataloader,
+                                           loss_fn=loss_fn,
+                                           optimizer=optimizer)
+
+        test_loss, test_acc = test_step(model=model,
+                                        dataloader=test_dataloader,
+                                        loss_fn=loss_fn)
+
+        #print out whats happening
+        print(
+            f"Epoch: {epoch + 1} | "
+            f"train_loss: {train_loss:.4f} | "
+            f"train_acc: {train_acc:.4f} | "
+            f"test_loss: {test_loss:.4f} | "
+            f"test_acc: {test_acc:.4f}"
+        )
+
+        #update results dictionary
+        results['train_loss'].append(train_loss)
+        results['train_acc'].append(test_acc)
+        results['test_loss'].append(test_loss)
+        results['test_acc'].append(test_acc)
+
+    return results
+
+# 7.7 Train and Evaluate Model 0
+# Time to put our TinyVGG model, DataLoader's and train() function together to see if we can build a model capable of discerning between pizza, steak and sushi!
+
+#set random seeds
+torch.manual_seed(42)
+torch.cuda.manual_seed(42)
+
+#set number of epochs
+NUM_EPOCHS = 5
+
+#recreate an instance on TinyVGG
+model_0 = TinyVGG(input_shape=3,  # number of color channels (3 for RGB)
+                  hidden_units=10,
+                  output_shape=len(train_data.classes)).to(device)
+
+#setup loss function and optimizer
+loss_fn = nn.CrossEntropyLoss()
+optimizer = torch.optim.Adam(params=model_0.parameters(), lr=0.001)
+
+#start timer
+from timeit import default_timer as timer
+start_timer = timer()
+
+#train model_0
+if __name__ == '__main__':
+    model_0_results = train(model=model_0,
+                            train_dataloader=train_dataloader_simple,
+                            test_dataloader=test_dataloader_simple,
+                            optimizer=optimizer,
+                            loss_fn=loss_fn,
+                            epochs=NUM_EPOCHS)
+
+    # End the timer and print out how long it took
+    end_time = timer()
+    print(f'Total training time {end_time-start_timer:.3f} seconds')
+
+    # 7.8 Plot the loss curves of Model 0
+    # Check the model_0_results keys
+    # print(model_0_results.keys())
+
+# We'll need to extract each of these keys and turn them into a plot.
+    def plot_loss_curves(results):
+        #get the loss values of the results dictionary (training and test)
+        loss = results['train_loss']
+        test_loss = results['test_loss']
+
+        #get the accuracy values of the results dictionary (training and test)
+        accuracy = results['train_acc']
+        test_accuracy = results['test_acc']
+
+        #figure out how many epochs there were
+        epochs = range(len(results['train_loss']))
+
+        #setup a plot
+        plt.figure(figsize=(15, 7))
+
+        #plot loss
+        plt.subplot(1, 2, 1)
+        plt.plot(epochs, loss, label='train_loss')
+        plt.plot(epochs, test_loss, label='test_loss')
+        plt.title('Loss')
+        plt.xlabel('Epochs')
+        plt.legend()
+
+
+        #plot accuracy
+        plt.subplot(1, 2, 2)
+        plt.plot(epochs, accuracy, label='train_accuracy')
+        plt.plot(epochs, test_accuracy, label='test_accuracy')
+        plt.title('Accuracy')
+        plt.xlabel('Epochs')
+        plt.legend()
+
+        plt.show()
+
+    plot_loss_curves(model_0_results)
+
+
+
+
+
 
 
 
