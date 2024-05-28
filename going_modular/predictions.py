2f31a176ee8ab504251549e99828ead541d619dc
Akyl <akyl@gmail.com>
2024-05-28 15:57:08 +0200

    31th commit

---
+++ going_modular/predictions.py
@@ -0,0 +1,55 @@
+from typing import List, Tuple
+import torch, torchvision
+from torchvision import transforms
+from PIL import Image
+import matplotlib.pyplot as plt
+
+device = 'cuda' if torch.cuda.is_available() else 'cpu'
+
+# 1. Take in a trained model, class names, image path, image size, a transform and target device
+def pred_and_plot_image(model: torch.nn.Module,
+                        image_path: str,
+                        class_names: List[str],
+                        image_size: Tuple[int, int] = (224, 224),
+                        transform: torchvision.transforms = None,
+                        device: torch.device = device):
+    # 2. Open image
+    img = Image.open(image_path)
+
+    # 3. Create transformation for image (if one doesn't exist)
+    if transform is not None:
+        image_transform = transform
+    else:
+        image_transform = transforms.Compose([
+            transforms.Resize(image_size),
+            transforms.ToTensor(),
+            transforms.Normalize(mean=[0.485, 0.456, 0.406],
+                                 std=[0.229, 0.224, 0.225]),
+        ])
+
+    ### Predict on image ###
+
+    # 4. Make sure the model is on the target device
+    model.to(device)
+
+    # 5. Turn on model evaluation mode and inference mode
+    model.eval()
+    with torch.inference_mode():
+        # 6. Transform and add an extra dimension to image (model requires samples in [batch_size, color_channels, height, width])
+        transformed_image = image_transform(img).unsqueeze(dim=0)
+
+        # 7. Make a prediction on image with an extra dimension and send it to the target device
+        target_image_pred = model(transformed_image.to(device))
+
+    # 8. Convert logits -> prediction probabilities (using torch.softmax() for multi-class classification)
+    target_image_pred_probs = torch.softmax(target_image_pred, dim=1)
+
+    # 9. Convert prediction probabilities -> prediction labels
+    target_image_pred_label = torch.argmax(target_image_pred_probs, dim=1)
+
+    # 10. Plot image with predicted label and probability
+    plt.figure()
+    plt.imshow(img)
+    plt.title(f"Pred: {class_names[target_image_pred_label]} | Prob: {target_image_pred_probs.max():.3f}")
+    plt.axis(False)
+    plt.show()
