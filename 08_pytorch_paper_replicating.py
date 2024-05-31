acd488b4fb26de53cc320b13bf2df1ae95cb4cff
Akyl <akyl@gmail.com>
2024-05-31 17:37:01 +0200

    35th commit

--- 08_pytorch_paper_replicating.py
+++ 08_pytorch_paper_replicating.py
@@ -184,6 +184,109 @@ conv2d = nn.Conv2d(in_channels=3, #number of colors channels
 # plt.axis(False)
 # plt.show()
 
+#pass the image through the convolutional layer
+image_out_of_conv = conv2d(image.unsqueeze(0)) # add a single batch dimension (height, width, color_channels) -> (batch, height, width, color_channels)
+# print(image_out_of_conv.shape)
+
+#plot random 5 convolutional features maps
+import random
+random_indexes = random.sample(range(0, 758), k=5)
+# print(f'showing random convolutional features maps from indexes {random_indexes}')
+#
+# #create plot
+# fig, axs = plt.subplots(nrows=1, ncols=5, figsize=(12, 12))
+#
+# #plot random image features maps
+# for i, idx in enumerate(random_indexes):
+#     image_conv_feature_map = image_out_of_conv[:, idx, :, :] #index on the output tensor of the convolutional layer
+#     axs[i].imshow(image_conv_feature_map.squeeze().detach().numpy())
+#     axs[i].set(xticklabels=[], yticklabels=[], xticks=[], yticks=[])
+# plt.show()
+
+#Create flatten layer
+flatten = nn.Flatten(start_dim=2, # flatten feature_map_height (dimension 2)
+                     end_dim=3) # flatten feature_map_width (dimension 3)
+
+#view single image
+# plt.imshow(image.permute(1, 2, 0))
+# plt.title(class_names[label])
+# plt.axis(False)
+# plt.show()
+# print(f'original image shape {image.shape}')
+
+#turn image into feature maps
+image_out_of_conv = conv2d(image.unsqueeze(0)) # add batch dimension to avoid shape errors
+# print(f'Image feature map shape: {image_out_of_conv.shape}')
+
+#flatten the feature maps
+image_out_of_conv_flattened = flatten(image_out_of_conv)
+# print(f'Flattened image feature map shape: {image_out_of_conv_flattened.shape}')
+
+# Get flattened image patch embeddings in right shape
+image_out_of_conv_flattened_reshaped = image_out_of_conv_flattened.permute(0, 2, 1)  # [batch_size, P^2•C, N] -> [batch_size, N, P^2•C]
+
+#get a single flattened feature map
+single_flattened_feature_map = image_out_of_conv_flattened_reshaped[:, :, 0] # index: (batch_size, number_of_patches, embedding_dimension)
+
+#plot the flattened feature map visually
+# plt.figure(figsize=(22, 22))
+# plt.imshow(single_flattened_feature_map.detach().numpy())
+# plt.title(f'Flattened feature map shape: {single_flattened_feature_map.shape}')
+# plt.axis(False)
+# plt.show()
+
+# 4.5 Turning the ViT patch embedding layer into a PyTorch module
+# Time to put everything we've done for creating the patch embedding into a single PyTorch layer.
+# We can do so by subclassing nn.Module and creating a small PyTorch "model" to do all of the steps above.
+# Specifically we'll:
+# Create a class called PatchEmbedding which subclasses nn.Module (so it can be used a PyTorch layer).
+# Initialize the class with the parameters in_channels=3, patch_size=16 (for ViT-Base) and embedding_dim=768 (this is  𝐷  for ViT-Base from Table 1).
+# Create a layer to turn an image into patches using nn.Conv2d() (just like in 4.3 above).
+# Create a layer to flatten the patch feature maps into a single dimension (just like in 4.4 above).
+# Define a forward() method to take an input and pass it through the layers created in 3 and 4.
+# Make sure the output shape reflects the required output shape of the ViT architecture ( 𝑁×(𝑃2⋅𝐶) ).
+
+class PatchEmbedding(nn.Module):
+    def __init__(self, in_channels: int=3,
+                 patch_size: int=16,
+                 embedding_dim: int=768):
+        super().__init__()
+        #create layer to turn an image into patches
+        self.patcher = nn.Conv2d(in_channels=in_channels,
+                                 out_channels=embedding_dim,
+                                 kernel_size=patch_size,
+                                 stride=patch_size,
+                                 padding=0)
+
+        #create a layer to flatten the patch feature maps into a single dimension
+        self.flatten = nn.Flatten(start_dim=2,# only flatten the feature map dimensions into a single vector
+                                  end_dim=3)
+
+    def forward(self, x):
+        #create assertion to check that inputs are the correct shape
+        image_resolution = x.shape[-1]
+        assert image_resolution % patch_size == 0, f'Input image size must be divisible by patch size, image shape:{image_resolution}, patch size:{patch_size}'
+
+        #perform the forward pass
+        x_patched = self.patcher(x)
+        x_flattened = self.flatten(x_patched)
+        #make sure that output shape has right order
+        return x_flattened.permute(0, 2, 1) # adjust so the embedding is on the final dimension [batch_size, P^2•C, N] -> [batch_size, N, P^2•C]
+
+set_seeds()
+
+#create an instance of patch embedding layer
+patchify = PatchEmbedding(in_channels=3,
+                          patch_size=16,
+                          embedding_dim=768)
+
+#pass the single image through
+# print(f'Input image shape {image.unsqueeze(0).shape}')
+patch_embedded_image = patchify(image.unsqueeze(0))# add an extra batch dimension on the 0th index, otherwise will error
+# print(f'output patch embedding shape {patch_embedded_image.shape}')
+
+
+
 
 
 
